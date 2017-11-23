9f93b0a9faeccaee41ac73814dc9ea854640a49b
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-11-23 21:40:33 +0100

    Ex-3

---
+++ Exercises2/Customer.java
@@ -0,0 +1,15 @@
+
+public class Customer {
+
+    public int customerID = 0;
+    public char status = 'N';
+    public double totalPurchases = 0.0;
+
+    public void displayCustomerInfo() {
+
+        System.out.println("Customer ID: " + customerID);
+        System.out.println("Status: " + status);
+        System.out.println("Purchases: " + totalPurchases);
+
+    }
+}
