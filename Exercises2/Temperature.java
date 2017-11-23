db67a2505744a7abe69e84d58ab2cf15eafdcae3
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-11-23 22:56:55 +0100

    Ex-3

---
+++ Exercises2/Temperature.java
@@ -0,0 +1,9 @@
+
+public class Temperature {
+    public float fahrenheitTemp = 78.9F;
+    
+    public void calculateCelsius(){
+
+        System.out.println((fahrenheitTemp - 32) * 5 / 9);
+    }
+}
