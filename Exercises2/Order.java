579b8e66ba5f4307f799591b13036e05c911cfb3
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-11-23 21:59:36 +0100

    Ex-3

---
+++ Exercises2/Order.java
@@ -0,0 +1,11 @@
+
+public class Order {
+    public long orderValue = 0L;
+    public int itemQuantity = 10_000_000;
+    public int itemPrice = 555_500;
+    
+    public void calculateTotal(){
+        orderValue = (long )itemQuantity * itemPrice;
+        System.out.println("Order total: "+ orderValue);
+    }
+}
