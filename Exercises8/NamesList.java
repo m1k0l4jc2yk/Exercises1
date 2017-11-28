839c39f7788572971ab87b236752ff7b59eca156
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-11-28 22:32:43 +0100

    Ex-8

---
+++ Exercises8/NamesList.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+public class NamesList {
+    public ArrayList listOfNames;
+    
+    public void setList(){
+        listOfNames = new ArrayList();
+        listOfNames.add("Joe Smith");
+        listOfNames.add("Mary Palmer");
+        listOfNames.add("Jose Gonzalez");
+        listOfNames.add("Linda Baker");
+        
+        System.out.println("Names List: " + listOfNames);
+        System.out.println("Size of ArrayList: " + listOfNames.size());
+    }
+    
+    public void manipulateList(){
+        listOfNames.remove("Joe Smith");
+        System.out.println ("Names list after removing element: " + listOfNames);
+        System.out.println ("Size of ArrayList: " + listOfNames.size());
+        listOfNames.add(1, "Joe Smith");
+        System.out.println ("Names list after adding element: " + listOfNames);
+        System.out.println ("Size of ArrayList: " + listOfNames.size());
+    }
+}
