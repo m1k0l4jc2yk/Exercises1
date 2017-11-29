e915fa5fe9b0c084d6e6b00ad9c793ca45db2724
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-11-29 21:53:38 +0100

    Ex-9

---
+++ Exercises9/Ex9.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Ex9 {
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbę");
+        int i1 = sc.nextInt();
+
+        System.out.println("Podaj pierwsza liczbę");
+        int i2 = sc.nextInt();
+
+        System.out.println("Podaj pierwsza liczbę");
+        int i3 = sc.nextInt();
+
+        System.out.println("Podaj pierwsza liczbę");
+        int i4 = sc.nextInt();
+
+        System.out.println("Srednia 4 podanych liczb = " + (i1 + i2 + i3 + i4) / 4);
+
+
+    }
+}
