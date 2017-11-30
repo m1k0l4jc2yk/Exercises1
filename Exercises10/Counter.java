e287f1676c7f3701b15ff244430a95e87280633b
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-11-30 20:34:15 +0100

    Ex-10

---
+++ Exercises10/Counter.java
@@ -0,0 +1,16 @@
+
+public class Counter {
+
+    public final int MAX_COUNT = 100;
+
+    public  void displayCount(){
+
+        for (int count =1; count <= MAX_COUNT; count++){
+            if (count % 12 == 0) {
+                System.out.println( count + " ");
+            }
+
+        }
+    }
+
+}
