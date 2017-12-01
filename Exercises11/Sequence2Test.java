4f35b94de4a3db11b9b258e0eebfec192ef5dbda
Krzysztof <m1k0l4jc2yk@users.noreply.github.com>
2017-12-01 20:24:02 +0100

    Ex-11

---
+++ Exercises11/Sequence2Test.java
@@ -0,0 +1,10 @@
+
+public class Sequence2Test {
+    public static void main (String args[]) {
+
+        Sequence2 mySequence = new Sequence2();
+
+        mySequence.displaySequence();
+
+    }
+}
